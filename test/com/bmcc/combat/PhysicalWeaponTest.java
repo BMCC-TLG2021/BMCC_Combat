2d8865b226ca430845e99e807fe3c5e91f8c61ff
PengChen11 <peng.chen11@outlook.com>
2021-08-26 11:19:05 -0700

    physical weapons are defined with JSON. AB#180 "

--- test/com/bmcc/combat/PhysicalWeaponTest.java
+++ test/com/bmcc/combat/PhysicalWeaponTest.java
@@ -6,26 +6,26 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PhysicalWeaponTest {
-    PhysicalWeapon simpleWeapon;
+    PhysicalWeapon samplePhysicalWeapon;
     @Before
     public void setUp() throws Exception {
-        simpleWeapon = PhysicalWeapon.getInstance("weapon name", 100.0);
+        samplePhysicalWeapon = PhysicalWeapon.getInstance("weapon name", 100.0);
     }
 
     @Test
     public void getName() {
-        assertEquals("weapon name", simpleWeapon.getName());
+        assertEquals("weapon name", samplePhysicalWeapon.getName());
     }
 
     @Test
     public void getDamage() {
-        assertEquals(100.0, simpleWeapon.getDamage(),1);
+        assertEquals(100.0, samplePhysicalWeapon.getDamage(),1);
     }
 
     @Test
     public void getInstanceFromJson() throws Exception {
-        simpleWeapon = PhysicalWeapon.getInstanceFromJson("asset/simplePhysicalWeapon.json");
-        assertEquals("Bad Ass Axe", simpleWeapon.getName());
-        assertEquals(22.0, simpleWeapon.getDamage(),1);
+        samplePhysicalWeapon = PhysicalWeapon.getInstanceFromJson("asset/samplePhysicalWeapon.json");
+        assertEquals("Bad Ass Axe", samplePhysicalWeapon.getName());
+        assertEquals(22.0, samplePhysicalWeapon.getDamage(),1);
     }
 }
