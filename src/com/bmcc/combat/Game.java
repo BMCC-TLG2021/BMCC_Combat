024cc1757f1125bc240491298159b0b6b2b6f89a
PengChen11 <peng.chen11@outlook.com>
2021-08-27 09:04:12 -0700

    Revert "conf test"
    This reverts commit 65a2795b3d6c26c4b73c30d1738605c9117f7c83.

--- src/com/bmcc/combat/Game.java
+++ src/com/bmcc/combat/Game.java
@@ -3,11 +3,6 @@ package com.bmcc.combat;
 public class Game {
     private Character character;       // character?? maybe player
 
-
-    public void test(){
-        //my test
-    }
-
     public void startGame() throws Exception {
 
         GameOutput.welcomePlayer();
