498e03bfaa6b9ab5c0e1377d0ba351587f4f88f9
Bikash Bhaila <bikashbhaila@gmail.com>
2021-08-27 08:54:57 -0700

    AB#101 AB#106 Added ability for user to enter character name and attack another player

--- src/com/bmcc/combat/Game.java
+++ src/com/bmcc/combat/Game.java
@@ -1,18 +1,26 @@
 package com.bmcc.combat;
 
 public class Game {
-    private Character character;       // character?? maybe player
+    private Character character;
 
-    public void startGame() throws Exception {
+    public void play() throws Exception {
+        welcomeUser();
+        setPlayers();
+    }
 
+    private void welcomeUser() {
         GameOutput.welcomePlayer();
         GameOutput.showInstructions();
-//        String userName = GameInput.getUserInput("Please enter name for your character:");
+    }
+
+    private void setPlayers() throws Exception {
+        String userName = GameInput.getUserInput("Please enter name for your character:");
+
+        Character userPlayer = new Character(userName, 100, 100, 10, 30);
+        Character enemyPlayer = new Character("KING", 100, 100, 12, 25);
 
         PhysicalWeapon pWeapon = PhysicalWeapon.getInstanceFromJson("asset/samplePhysicalWeapon.json");
         MagicalWeapon mWeapon = MagicalWeapon.getInstanceFromJson("asset/sampleMagicalWeapon.json");
-        Character userPlayer = new Character("userName", 100, 100, 10, 30);
-        Character enemyPlayer = new Character("KING", 100, 100, 12, 25);
 
 //        userPlayer.setWeapon(pWeapon);
         userPlayer.setWeapon(mWeapon);
@@ -20,42 +28,58 @@ public class Game {
 
         userPlayer.setMagic(magic);
 
-        String command = GameInput.getCommand();
-
-        if (command.equals("ATTACK ENEMY")) {
-            int damagePoint = userPlayer.getTotalPhysicalAttackPower() - enemyPlayer.getDefensePower();
-            enemyPlayer.damage(damagePoint);
-            System.out.println(userPlayer);
-            System.out.println(enemyPlayer);
-            Thread.sleep(3000);
-
-            int enemyDamagePoint = enemyPlayer.getTotalPhysicalAttackPower() - userPlayer.getDefensePower();
-            userPlayer.damage(enemyDamagePoint);
-            System.out.println(userPlayer);
-            System.out.println(enemyPlayer);
-        }
-
-        if (command.equals("USE MAGIC")) {
-            int damagePoint = userPlayer.getTotalMagicalPower();
-
-            if (userPlayer.reduceMagicPoint()) {
-                enemyPlayer.damage(damagePoint);
-                System.out.println("Magic Worked!!");
-            }
-            else {
-                System.out.println("Player does not have enough Magic Power..");
-            }
-
-            Thread.sleep(3000);
-
-            int enemyDamagePoint = enemyPlayer.getTotalPhysicalAttackPower() - userPlayer.getDefensePower();
-            userPlayer.damage(enemyDamagePoint);
-            System.out.println(userPlayer);
-            System.out.println(enemyPlayer);
-        }
-
-
-
-
+        controlFlow(userPlayer, enemyPlayer);
     }
+
+
+    private void controlFlow(Character userPlayer, Character enemyPlayer) throws Exception {
+        while (true) {
+            String command = GameInput.getCommand();
+            switch (command) {
+                case "ATTACK ENEMY":
+                    attackEnemy(userPlayer, enemyPlayer);
+                    break;
+                case "USE MAGIC":
+                    useMagic(userPlayer, enemyPlayer);
+                    break;
+                case "END GAME":
+                    System.out.println("GoodBye.....");
+                    System.exit(0);
+            }
+        }
+    }
+
+    private void useMagic(Character userPlayer, Character enemyPlayer) throws InterruptedException {
+        int damagePoint = userPlayer.getTotalMagicalPower();
+
+        if (userPlayer.reduceMagicPoint()) {
+            enemyPlayer.damage(damagePoint);
+            System.out.println("Magic Worked!!");
+        } else {
+            System.out.println("Player does not have enough Magic Power..");
+        }
+
+        Thread.sleep(3000);
+
+        int enemyDamagePoint = enemyPlayer.getTotalPhysicalAttackPower() - userPlayer.getDefensePower();
+        userPlayer.damage(enemyDamagePoint);
+        System.out.println(userPlayer);
+        System.out.println(enemyPlayer);
+    }
+
+
+    private void attackEnemy(Character userPlayer, Character enemyPlayer) throws InterruptedException {
+        int damagePoint = userPlayer.getTotalPhysicalAttackPower() - enemyPlayer.getDefensePower();
+        enemyPlayer.damage(damagePoint);
+        System.out.println(userPlayer);
+        System.out.println(enemyPlayer);
+        Thread.sleep(3000);
+
+        int enemyDamagePoint = enemyPlayer.getTotalPhysicalAttackPower() - userPlayer.getDefensePower();
+        userPlayer.damage(enemyDamagePoint);
+        System.out.println(userPlayer);
+        System.out.println(enemyPlayer);
+    }
+
+//    private void decideWinner()
 }
