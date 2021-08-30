eab2d3bdeced3d35088cd8630ac4d63ff1514b3a
ychang0821 <changyun0821@gmail.com>
2021-08-30 15:55:07 -0700

    methods to parse JSON array of Healing items, magics added, corresponding test methods added, AB#182

--- src/com/bmcc/model/item/HealingItem.java
+++ src/com/bmcc/model/item/HealingItem.java
@@ -1,16 +1,23 @@
 package com.bmcc.model.item;
 
+import com.bmcc.model.skill.Magic;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.File;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
 
 public class HealingItem extends Item {
-
+    @JsonProperty("healHP")
     private final int healHP;
+    @JsonProperty("healMP")
     private final int healMP;
 
-    private HealingItem(String name, int healHP, int healMP){
+    private HealingItem(@JsonProperty("name") String name,@JsonProperty("healHP") int healHP, @JsonProperty("healMP")int healMP){
         super(name);
         this.healHP = healHP;
         this.healMP = healMP;
@@ -32,6 +39,17 @@ public class HealingItem extends Item {
         int healMP = (int)(long)jo.get("healMP");
 
         return new HealingItem(name, healHP, healMP);
+    }
+
+    public static List<HealingItem> getHealItemListFromJsonFile(String fileName) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<HealingItem> healingItemList = Arrays.asList(mapper.readValue(new File(fileName), HealingItem[].class));
+            return healingItemList;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static HealingItem getInstance(String name, int healHP, int healMP){
--- src/com/bmcc/model/skill/Magic.java
+++ src/com/bmcc/model/skill/Magic.java
@@ -1,15 +1,25 @@
 package com.bmcc.model.skill;
 
+import com.bmcc.model.character.Character;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.File;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
 
 public class Magic {
+    @JsonProperty("name") // optional
     private final String name;
+    @JsonProperty("damage") // optional
     private final int damage;
 
-    private Magic(String name, int damage){
+    @JsonCreator
+    private Magic(@JsonProperty("name") String name, @JsonProperty("damage") int damage){
         this.name = name;
         this.damage = damage;
     }
@@ -25,6 +35,17 @@ public class Magic {
         int damage = (int)(long) jo.get("damage");
 
         return new Magic(name, damage);
+    }
+
+    public static List<Magic> getMagicListFromJsonFile(String fileName) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Magic> magicList = Arrays.asList(mapper.readValue(new File(fileName), Magic[].class));
+            return magicList;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static Magic getInstance(String name, int damage){
