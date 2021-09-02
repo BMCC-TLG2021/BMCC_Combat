095ad94727e111787dacce561e1554caf8676a11
Bikash Bhaila <bikashbhaila@gmail.com>
2021-09-02 12:30:09 -0700

    updated equipment and its subclasses to add maxIntegrity and moneyValue AB#310 & AB312

--- src/com/bmcc/model/equipment/Equipment.java
+++ src/com/bmcc/model/equipment/Equipment.java
@@ -4,32 +4,47 @@ public class Equipment {
 
     private final String name;
     private int integrity;
+    private final int maxIntegrity;
     private final String desc;
+    private final int moneyValue;
 
 
-    Equipment(String name, int integrity, String desc) {
+    Equipment(String name, int integrity, int maxIntegrity, String desc, int moneyValue) {
         this.name = name;
         this.integrity = integrity;
+        this.maxIntegrity = maxIntegrity;
         this.desc = desc;
+        this.moneyValue = moneyValue;
     }
 
     public String getName(){
         return this.name;
     };
 
-
     public int getIntegrity(){
         return this.integrity;
     };
 
+    public int getMaxIntegrity() {
+        return maxIntegrity;
+    }
+
     public String getDesc(){
         return this.desc;
+    }
+
+    public int getMoneyValue() {
+        return moneyValue;
     }
 
     public void reduceIntegrity(){
         if (this.getIntegrity() != 0){
             this.integrity -= 1;
         }
+    }
+
+    public void restoreIntegrity() {
+        this.integrity = maxIntegrity;
     }
 
 }
